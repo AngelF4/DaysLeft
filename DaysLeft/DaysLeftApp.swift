e90d1262b792b9ba9dc457c2cab90f4f5ebc6794
AngelF4 <angel.hdz.g@icloud.com>
2025-07-23 12:32:37 -0600

    Initial Commit

---
+++ DaysLeft/DaysLeftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysLeftApp.swift
+//  DaysLeft
+//
+//  Created by Angel Hernández Gámez on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DaysLeftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
